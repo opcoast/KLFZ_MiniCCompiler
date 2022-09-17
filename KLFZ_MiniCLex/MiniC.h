4aebeb14216250d6dd3d47acab00319e09ad3c55
Qisumi <940858763@qq.com>
2022-09-17 13:31:27 +0800

    update 9.17

--- KLFZ_MiniCLex/MiniC.h
+++ KLFZ_MiniCLex/MiniC.h
@@ -6,10 +6,5 @@ using std::string, std::unordered_map, std::unordered_set;
 
 namespace MiniC
 {
-public:
-	static unordered_map<string, string> CHAR_MAP;
-	static unordered_map<string, int> TOKEN_ID;
-	static unordered_map<string, int> DFA_STATE_ID;
-};
 
-}
+};
